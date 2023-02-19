02441712f5ee215883619b7de632a81e7511e9c4
codewithsarthak102 <sarthakpandat781@gmail.com>
2023-02-19 12:09:02 +0530

    Initial Commit

---
+++ src/evenNumberGreaterThan5usinglist/EvenNumber.java
@@ -0,0 +1,19 @@
+package evenNumberGreaterThan5usinglist;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(6);
+        list.add(9);
+        list.add(12);
+        list.add(15);
+        list.add(16);
+
+        // Even Number > 5 [6,12,16]
+        System.out.println(list.stream().filter(y ->(y % 2 ==0 && y > 5)).collect(Collectors.toList()));
+    }
+}
