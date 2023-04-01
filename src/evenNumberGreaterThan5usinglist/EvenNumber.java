48dcd15c2b41abc49f2b2dcb0941376930650519
codewithsarthak102 <sarthakpandat781@gmail.com>
2023-04-01 07:40:38 +0530

    Initial Commit

--- src/evenNumberGreaterThan5usinglist/EvenNumber.java
+++ src/evenNumberGreaterThan5usinglist/EvenNumber.java
@@ -12,6 +12,9 @@ public class EvenNumber {
         list.add(12);
         list.add(15);
         list.add(16);
+        list.add(19);
+        list.add(18);
+        list.add(20);
         System.out.println(list);
         // Even Number > 5 [6,12,16]
         System.out.println(list.stream().filter(y ->(y % 2 ==0 && y > 5)).collect(Collectors.toList()));
